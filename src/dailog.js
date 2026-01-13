4f762a914dfc1b4ae173ef291deba673bcef2ce7
mohamad alsahwa <>
2026-01-13 17:37:05 +0200

    .

---
+++ src/dailog.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. `,
+
+  tv: `That's my TV. I've been watching tech youtubers Linus Tech Tips since I was a kid.`,
+
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+
+  resume: `This is my desk and on it is my resume. <a href="https://mohamad-alshawa.netlify.app/" target="_blank">Check it out?</a>
+  Contact me at mohamad.alchawwa@gmail.com `,
+
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. .`,
+
+  library: `There are a lot of programming books on my shelves. There is even one in Arabic (I also speak Arabic btw).`,
+  
+  exit: `If you want to exit Shawa's portfolio, just close the tab.`,
+};
